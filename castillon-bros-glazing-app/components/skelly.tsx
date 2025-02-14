e2a24ab1cf8afe36a176cd486f0bb7d5e4684dd2
JaylenLuc <jaylenluc1@gmail.com>
2025-02-14 10:46:15 +0000

    mvp of home

--- castillon-bros-glazing-app/components/skelly.tsx
+++ castillon-bros-glazing-app/components/skelly.tsx
@@ -3,27 +3,28 @@ import { Separator } from "@/components/ui/separator";
 export function Skelly() {
   return (
     <>
+        <h1 className="text-2xl font-bold text-center text-primary">Our Services</h1>
         <div className="flex items-center space-x-4 ">
             <div className="h-12 w-12 rounded-full bg-primary" />
             <div className="space-y-2 text-primary">
                 fjdkalfjdsakfl;dsafjkdsalf;djskfldsa;fjdskaf;ladsfj
             </div>
         </div>
-        <Separator className="my-1 mt-1 mb-1" />
+        <Separator className="my-2 mt-2 mb-2" />
         <div className="flex items-center space-x-4 ">
             <div className="h-12 w-12 rounded-full bg-primary" />
             <div className="space-y-2 text-primary">
                 fjdkalfjdsakfl;dsafjkdsalf;djskfldsa;fjdskaf;ladsfj
             </div>
         </div>
-        <Separator className="my-1 mt-1 mb-1" />
+        <Separator className="my-2 mt-2 mb-2" />
         <div className="flex items-center space-x-4 ">
             <div className="h-12 w-12 rounded-full bg-primary" />
             <div className="space-y-2 text-primary">
                 fjdkalfjdsakfl;dsafjkdsalf;djskfldsa;fjdskaf;ladsfj
             </div>
         </div>
-        <Separator className="my-1 mt-1 mb-1" />
+        <Separator className="my-2 mt-2 mb-2" />
         <div className="flex items-center space-x-4 ">
             <div className="h-12 w-12 rounded-full bg-primary" />
             <div className="space-y-2 text-primary">
--- castillon-bros-glazing-app/components/slider.tsx
+++ castillon-bros-glazing-app/components/slider.tsx
@@ -13,16 +13,16 @@ import { Separator } from "@/components/ui/separator";
 export function Slider() {
   return (
     <>
-        <Carousel className="w-full max-w-md">
+        <Carousel className="w-full max-w-l">
         <CarouselContent>
             {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} className="text-center">
-                <div className="p-1">
+            <CarouselItem key={index} className="text-center md:basis-1/2 lg:basis-1/3">
+                <div className="p-1 ">
                 <Card>
                     <CardContent className="flex aspect-square items-center justify-center p-6">
                     </CardContent>
                 </Card>
-                <span className="text-4xl font-semibold text-primary ">{index + 1} Image and text</span>
+                <span className="text-xl font-semibold text-primary ">{index + 1} Image and text</span>
                 </div>
             </CarouselItem>
             ))}
